b3b4cedde4bf623880df0fedc70223f399911fbc
Melvin1Atieno <melvinatieno@gmail.com>
2018-09-14 20:36:52 +0300

    Fix tests

--- tests/test_password_checker.py
+++ tests/test_password_checker.py
@@ -1,29 +1,31 @@
+from password_checker import check_password
+
 import unittest
 
 class TestPasswordChecker(unittest.TestCase):
 
     def test_password_checker_checks_for_atleast_one_letter_between_a_z(self):
-        results = check_password(1234567890)
+        results = check_password("12345A#")
         self.assertEqual("password must have atleast 1 letter between a-z", results)
 
     def test_password_checker_checks_for_atleast_one_number_between_0_9(self):
-        results = check_password("anotherpassword")
+        results = check_password("abcdC#$")
         self.assertEqual("password must have atleast one number between 0-9",results)
 
     def test_password_checker_checks_for_letters_between_A_Z(self):
-        results = check_password("another password")
+        results = check_password("abcd#$12")
         self.assertEqual("password must have atleast one letter between A-Z",results)
     
     def test_checker_checks_for_a_character(self):
-        results = check_password("another password")
+        results = check_password("123abcAD")
         self.assertEqual("password must have atleast one character from #$@",results)
 
     def test_password_checker_checks_for_minimum_length_of_6_characters(self):
-        results = check_password("pass")
+        results = check_password("1aAB#")
         self.assertEqual("minimum length should be 6 characters",results)
 
     def test_password_checker_checks_for_maximum_length_of_12(self):
-        results = check_password("anotherlongpassword")
+        results = check_password("aA1@asdgxcfer")
         self.assertEqual("maximum password length 12",results)
 
     def test_password_checker_returns_comma_separated_valid_passwords(self):
