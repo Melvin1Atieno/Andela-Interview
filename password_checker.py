4879408414cd229b867e6a70a2d871c7598ae207
Melvin1Atieno <melvinatieno@gmail.com>
2018-09-15 15:48:14 +0300

    Add code for password checker

--- password_checker.py
+++ password_checker.py
@@ -3,37 +3,37 @@ import re
 def check_password(password):
     """ chek password method"""
     passwords = password.split(",")
-    valid_passwords = ""
     for password in passwords:
         if len(password) < 6:
             errormsg = "minimum length should be 6 characters"
             passwords.remove(password)
-        elif len(password) > 12:
+        if len(password) > 12:
             errormsg = "maximum password length 12"
-            password.remove(password)
-        elif not re.search(r'[a-z]+',password):
+            passwords.remove(password)
+        if not re.search(r'[a-z]+',password):
             errormsg = "password must have atleast 1 letter between a-z"
             passwords.remove(password)
-        elif not re.search (r'[A-Z]+',password):
+        if not re.search (r'[A-Z]+',password):
             errormsg = "password must have atleast one letter between A-Z"
             passwords.remove(password)
-        elif not re.search(r'[0-9]', password):
+        if not re.search(r'[0-9]', password):
             errormsg = "password must have atleast one number between 0-9"
             passwords.remove(password)
-        elif not re.search(r'\W+', password):
+        if not re.search(r'\W+', password):
             errormsg = "password must have atleast one character from #$@"
             passwords.remove(password)
-        else:
-            passwords = "".join(passwords)
-    if errormsg:
-       return errormsg
+    if passwords:
+        return ",".join(passwords)
     else:
-        return passwords
+        return errormsg
+    # else:
+    #     return errormsg
 
     
         
 
-myname = "myname"
+myname = "ABd12341"
+
 
 print(check_password(myname))
         
