a138773c118703e47a04b163faa620c99de11ddc
Melvin1Atieno <melvinatieno@gmail.com>
2018-09-18 14:18:55 +0300

    Add code for tripple check

--- tests/test_tripple_check.py
+++ tests/test_tripple_check.py
@@ -6,7 +6,7 @@ class TestTrippleCheck(unittest.TestCase):
 
     def test_tripple_check_returns_desired_output(self):
         results = tripple_check([5, 3, 4, 3, 5, 5, 3])
-        self.assertEqual(4,results)
+        self.assertEqual('4',results)
 
     def test_tripple_check_returns_msg_for_list_with_all_tripples(self):
         results = tripple_check([5,3,3,3,4,5,4,5,4])
@@ -14,7 +14,7 @@ class TestTrippleCheck(unittest.TestCase):
 
     def test_tripple_check_return_comma_seperated_integers_for_values_not_in_trippl(self):
         results = tripple_check([5, 3, 4, 3, 5, 5, 3,7])
-        self.assertEqual((4,7),results)
+        self.assertEqual('4,7',results)
 
 if __name__ == "__main__":
     unittest.main()
--- tripple_check.py
+++ tripple_check.py
@@ -1,13 +1,15 @@
 
 
 def tripple_check(input_list):
+    non_tripples = ''
     for item in input_list:
         item_count = input_list.count(item)
         if item_count == 3:
             input_list = [items for items in input_list if items != item]
     if input_list:
-        return input_list[0]
+        return ",".join(map(str,input_list))
+    else:
+        return "List Contains all tripples"
 
 
 
-print(tripple_check([5, 3, 4, 3, 5, 5, 3]))
