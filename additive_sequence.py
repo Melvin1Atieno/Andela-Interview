3496fd77db5c53ffbba0a8354c25d8a34183bbe4
Melvin1Atieno <melvinatieno@gmail.com>
2018-09-18 18:07:02 +0300

    add additive sequence function

--- additive_sequence.py
+++ additive_sequence.py
@@ -1,3 +1,19 @@
 
 def additive_sequence(sequence):
-    
+    is_additive_sequence = True
+    if not all(isinstance(item,int) for item in sequence):
+        return False
+    if len(sequence) < 3:
+        return False
+    if sequence[0] + sequence[1] == sequence[2] and len(sequence)>3:
+        is_additive_sequence = True
+        return(additive_sequence(sequence[1:]))
+    if sequence[0] + sequence[1] != sequence[2]:
+        is_additive_sequence = False
+        return False
+    else:
+        return is_additive_sequence
+        
+print(additive_sequence([2,11, 13]))
+
+        
--- tests/test_additive_sequence.py
+++ tests/test_additive_sequence.py
@@ -1,13 +1,23 @@
+from additive_sequence import additive_sequence
+
 import unittest
 
 class TestAdditiveSequence(unittest.TestCase):
 
     def test_additive_sequence_returns_true_for_additive_sequence(self):
-        results = additive_sequence( 6, 6, 12, 18, 30)
+        results = additive_sequence([6, 6, 12, 18, 30])
         self.assertEqual(True,results)
 
     def test_additive_sequence_returns_false_for_additive_sequence(self):
-        results = additive_sequence( 6, 6, 12, 18, 30)
+        results = additive_sequence([6, 6, 1, 18, 30])
+        self.assertEqual(False,results)
+
+    def test_additive_sequence_only_accepts_a_list_of_integers(self):
+        results = additive_sequence([1,2,'a','b','c'])
+        self.assertEqual(False,results)
+    
+    def test_additive_sequence_returns_false_for_ist_of_two(self):
+        results = additive_sequence([1,2])
         self.assertEqual(False,results)
 
 
