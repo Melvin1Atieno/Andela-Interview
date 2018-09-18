3eb77987797aefc6ebcd2e584a63efbc8d768ba6
Melvin1Atieno <melvinatieno@gmail.com>
2018-09-18 13:21:47 +0300

    Remove typos from tests

--- tests/test_tripple_check.py
+++ tests/test_tripple_check.py
@@ -3,7 +3,7 @@ import unittest
 class TestTrippleCheck(unittest.TestCase):
 
 
-    def test_tripple_check_returns_desired_output_output(self):
+    def test_tripple_check_returns_desired_output(self):
         resuts = tripple_check([5, 3, 4, 3, 5, 5, 3])
         self.assertEqual(4,results)
 
